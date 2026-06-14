28af6d33f45fe72ff542047c7d67390350c67485
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-06-14 15:46:11 +0530

    Completed OCP exercise

---
+++ Module-01-Design-Principles-Patterns/SOLID/OCP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IPayment
+{
+    void Pay();
+}
+
+class CreditCardPayment : IPayment
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment made using Credit Card");
+    }
+}
+
+class UpiPayment : IPayment
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment made using UPI");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IPayment payment1 = new CreditCardPayment();
+        payment1.Pay();
+
+        IPayment payment2 = new UpiPayment();
+        payment2.Pay();
+    }
+}
---
+++ Module-01-Design-Principles-Patterns/SOLID/OCP/README.md.txt
@@ -0,0 +1,11 @@
+# Open Closed Principle (OCP)
+
+A software entity should be open for extension but closed for modification.
+
+IPayment acts as an abstraction.
+
+CreditCardPayment and UpiPayment extend functionality without modifying existing code.
+
+Output:
+Payment made using Credit Card
+Payment made using UPI
