202fed729ee9464b2b951c17780f4eb72faaf571
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-06-15 20:12:13 +0530

    Completed Adapter Pattern

---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+interface IMediaPlayer
+{
+    void Play(string audioType, string fileName);
+}
+
+class AdvancedMediaPlayer
+{
+    public void PlayMp4(string fileName)
+    {
+        Console.WriteLine("Playing MP4 file: " + fileName);
+    }
+}
+
+class MediaAdapter : IMediaPlayer
+{
+    private AdvancedMediaPlayer advancedPlayer;
+
+    public MediaAdapter()
+    {
+        advancedPlayer = new AdvancedMediaPlayer();
+    }
+
+    public void Play(string audioType, string fileName)
+    {
+        if (audioType.Equals("mp4"))
+        {
+            advancedPlayer.PlayMp4(fileName);
+        }
+    }
+}
+
+class AudioPlayer : IMediaPlayer
+{
+    private MediaAdapter adapter;
+
+    public void Play(string audioType, string fileName)
+    {
+        if (audioType.Equals("mp3"))
+        {
+            Console.WriteLine("Playing MP3 file: " + fileName);
+        }
+        else if (audioType.Equals("mp4"))
+        {
+            adapter = new MediaAdapter();
+            adapter.Play(audioType, fileName);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AudioPlayer player = new AudioPlayer();
+
+        player.Play("mp3", "song.mp3");
+        player.Play("mp4", "video.mp4");
+    }
+}
---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Adapter/README.md.txt
@@ -0,0 +1,12 @@
+# Adapter Design Pattern
+
+Adapter Pattern allows incompatible interfaces to work together.
+
+Classes:
+- AudioPlayer
+- MediaAdapter
+- AdvancedMediaPlayer
+
+Output:
+Playing MP3 file: song.mp3
+Playing MP4 file: video.mp4
