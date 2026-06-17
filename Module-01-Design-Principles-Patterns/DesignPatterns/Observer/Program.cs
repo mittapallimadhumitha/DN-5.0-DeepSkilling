334668410f70e8fc6b6f9a9b630b03d925fad87e
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-06-17 19:09:24 +0530

    Completed Observer Pattern

---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Observer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IObserver
+{
+    void Update(string message);
+}
+
+class Subscriber : IObserver
+{
+    private string name;
+
+    public Subscriber(string name)
+    {
+        this.name = name;
+    }
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"{name} received: {message}");
+    }
+}
+
+class YouTubeChannel
+{
+    private List<IObserver> subscribers = new List<IObserver>();
+
+    public void Subscribe(IObserver observer)
+    {
+        subscribers.Add(observer);
+    }
+
+    public void NotifySubscribers(string videoTitle)
+    {
+        foreach (var subscriber in subscribers)
+        {
+            subscriber.Update(videoTitle);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        YouTubeChannel channel = new YouTubeChannel();
+
+        Subscriber s1 = new Subscriber("Madhumitha");
+        Subscriber s2 = new Subscriber("Rahul");
+
+        channel.Subscribe(s1);
+        channel.Subscribe(s2);
+
+        channel.NotifySubscribers("New C# Tutorial Uploaded");
+    }
+}
---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Observer/README.md.txt
@@ -0,0 +1,12 @@
+# Observer Design Pattern
+
+Observer Pattern establishes a one-to-many relationship.
+
+Classes:
+- IObserver
+- Subscriber
+- YouTubeChannel
+
+Output:
+Madhumitha received: New C# Tutorial Uploaded
+Rahul received: New C# Tutorial Uploaded
