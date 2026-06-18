8e08784c74f55a0f201f95883a90851d9ccfa3d4
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-06-18 21:46:41 +0530

    Completed Command Pattern

---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Command/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+interface ICommand
+{
+    void Execute();
+}
+
+class Light
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Light is ON");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Light is OFF");
+    }
+}
+
+class LightOnCommand : ICommand
+{
+    private Light light;
+
+    public LightOnCommand(Light light)
+    {
+        this.light = light;
+    }
+
+    public void Execute()
+    {
+        light.TurnOn();
+    }
+}
+
+class LightOffCommand : ICommand
+{
+    private Light light;
+
+    public LightOffCommand(Light light)
+    {
+        this.light = light;
+    }
+
+    public void Execute()
+    {
+        light.TurnOff();
+    }
+}
+
+class RemoteControl
+{
+    private ICommand command;
+
+    public void SetCommand(ICommand command)
+    {
+        this.command = command;
+    }
+
+    public void PressButton()
+    {
+        command.Execute();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Light light = new Light();
+
+        ICommand lightOn = new LightOnCommand(light);
+        ICommand lightOff = new LightOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.SetCommand(lightOn);
+        remote.PressButton();
+
+        remote.SetCommand(lightOff);
+        remote.PressButton();
+    }
+}
---
+++ Module-01-Design-Principles-Patterns/DesignPatterns/Command/README.md.txt
@@ -0,0 +1,14 @@
+# Command Design Pattern
+
+Command Pattern encapsulates a request as an object.
+
+Classes:
+- ICommand
+- Light
+- LightOnCommand
+- LightOffCommand
+- RemoteControl
+
+Output:
+Light is ON
+Light is OFF
