bbd93ea628b1573d60b520c20c2b3a5d267cb4de
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-07-02 12:01:26 +0530

    Completed Stack

---
+++ Module-02-DSA/DSA/Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+
+        Console.WriteLine("Stack Elements:");
+
+        foreach (int item in stack)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("\nTop Element: " + stack.Peek());
+
+        Console.WriteLine("Removed Element: " + stack.Pop());
+
+        Console.WriteLine("\nStack After Pop:");
+
+        foreach (int item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Module-02-DSA/DSA/Stack/README.md.txt
@@ -0,0 +1,25 @@
+# Stack
+
+## Definition
+
+A Stack is a linear data structure that follows the Last In First Out (LIFO) principle.
+
+## Operations Performed
+
+- Push
+- Peek
+- Pop
+
+## Output
+
+Stack Elements:
+30
+20
+10
+
+Top Element: 30
+Removed Element: 30
+
+Stack After Pop:
+20
+10
