681447551e4f27eccec8e1b56b659d0133e4f921
Mittapalli Madhumitha <mittapallimadhumitha123@example.com>
2026-07-19 15:10:19 +0530

    Completed

---
+++ Module-04-NUnit-Moq/Moq-Handson/CustomerCommLib/MailSender.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace CustomerCommLib
+{
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("your_email@gmail.com");
+
+                mail.To.Add(toAddress);
+
+                mail.Subject = "Test Mail";
+
+                mail.Body = message;
+
+                smtp.Port = 587;
+
+                smtp.Credentials =
+                    new NetworkCredential("username", "password");
+
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
